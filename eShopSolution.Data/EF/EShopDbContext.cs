c4ce1767b335d97385653c379c520ae8849fd92e
hungntt2803 <hungntt2803@gmail.com>
2022-04-05 20:55:35 +0700

    create entities

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<Contact> contacts { get; set; }
+        public DbSet<Language> languages { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderDetail> orderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductTranslation> productTranslations { get; set; }
+        public DbSet<Promotion> promotions { get; set; }
+        public DbSet<Transaction> transactions { get; set; }
+
+    }
+}
